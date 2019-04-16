2b3fc95a747cd8291c5812d258b70513c614bfa5
Lucas Santos <lukinha.g12.g12@gmail.com>
2019-04-16 12:03:53 -0300

    commit do meno global do crud

---
+++ menu_global_crud.php
@@ -0,0 +1,37 @@
+﻿<?php
+    //MENU GLOBAL DO CRUD DE REGISTRO DE PESSOAS	
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	</head>
+	
+    <body>
+		<div id="menu">
+			
+			<div>
+				
+				<hr width="100%"/>
+			
+				<ul>
+					
+					<li>
+						<a href="ins_registro_de_pessoas.php">Cadastrar Registro</a>
+					</li>
+			
+					<li>
+						<a href="consulta_registro.php">Consultar Registro</a>
+					</li>
+					
+				</ul>
+				
+				<hr width="100%"/>
+
+			</div>	
+			
+		</div>	
+		
+	</body>
+</html>				
