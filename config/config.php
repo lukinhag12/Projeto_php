2afafe137fa77793de256a2f80a3f08f0d60acec
Lucas Santos <lukinha.g12.g12@gmail.com>
2019-04-16 08:50:48 -0300

    commit include configuração com o banco de dados

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$localDB = 'localhost';
+$userDB = 'root';
+$senhaDB = '';
+$db_aplicacao = 'projeto_php';
+$conexao = mysql_connect($localDB,$userDB,$senhaDB) or die ('erro na conexão');
+mysql_select_db($db_aplicacao) or die ('erro na seleção do DB');
+
+?>
